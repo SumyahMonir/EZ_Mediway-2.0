1a62a90f792889415d05ba6322645f2088045080
Sumyah Monir Mithy <sumaiyamonir2003@gmail.com>
2026-08-13 21:52:13 +0600

    Bishmillah

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,13 @@ const allowedOrigins = [
   process.env.CLIENT_URL,
   "http://localhost:5173",
   "http://192.168.1.105:5173",
-]
+  "https://ez-mediway-2-0.vercel.app",
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
 
 app.use(cors({
   origin: allowedOrigins,
